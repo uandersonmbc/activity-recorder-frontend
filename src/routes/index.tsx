3c5f9ca0fd746d99f6502d5c02e9ac9871010cee
Uanderson <uandersonmbc@hotmail.com>
2020-12-12 00:04:38 -0300

    subindo alterações

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Dashboard from '../screens/Dashboard';
+import Reports from '../screens/Reports';
 import SingIn from '../screens/SingIn';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/login" exact={true}>
+      <Route path="/login">
         <SingIn />
       </Route>
-      <Route path="/dashboard" exact={true}>
+
+      <Route path="/dashboard">
         <Dashboard />
+      </Route>
+
+      <Route path="/reports">
+        <Reports />
       </Route>
     </BrowserRouter>
   );
---
+++ src/screens/Reports/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reports: React.FC = () => {
+  return <div>Relatórios</div>;
+};
+
+export default Reports;
